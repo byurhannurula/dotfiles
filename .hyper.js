dc2398e6ccc55188c94b815da405afdb401bf85e
Byurhan Beyzat <imbyurhan@gmail.com>
2018-07-20 16:00:12 +0300

    Initial commit 🚀

---
+++ .hyper.js
@@ -0,0 +1,14 @@
+module.exports = {
+  updateChannel: 'canary',
+  config: {
+    fontSize: 20,
+    fontFamily: '"Operator Mono", "Inconsolata for Powerline", Hack, Menlo monospace',
+    cursorShape: 'BLOCK',
+    cursorBlink: false,
+    wickedBorder: true,
+    padding: '20px',
+    shell: '/bin/zsh'
+  },
+
+  plugins: [],
+};
